2696f0799d1e17428d0298901e821d260ca34f4f
afp <48381990+afp05@users.noreply.github.com>
2024-11-11 18:55:27 +0100

    Add files via upload

---
+++ SimpeException/SimpeException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpeException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================== !!! Exceptions !!! ==================" );
+            Car mycar = new Car("ZAZ", 29);
+            mycar.CrankTunes(true);
+            try
+            {
+                for (int i = 0; i < 910; i++) mycar.Accelerate(10);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine("Method: {0} ", e.TargetSite);
+                Console.WriteLine("Message; {0} ", e.Message);
+                Console.WriteLine("Source : {0} ", e.Source);
+                Console.WriteLine("--------end of the error-----------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SimpeException/SimpeException/Radio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpeException
+{
+    class Radio
+    {
+        public void TurnOn (bool on)
+        {
+            if (on) Console.WriteLine("Playing music");
+            else Console.WriteLine("Radio is turned off. Nothing happens");
+        }
+    }
+}
